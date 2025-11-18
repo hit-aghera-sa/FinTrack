9de3e151058c6e63756a692085d4bab3289819a2
Hit Aghera <heetaghera809@gmail.com>
2025-11-18 09:30:03 +0530

    add rateLimit to limit  requests from same api, create helmet to add http security header, add mongo-sanitize and xss for data sanitization

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,31 @@
-const app = require("./server.js")
+const app = require("./server.js");
 const userRoute = require("./routers/users.route.js");
 const AppError = require("./utils/appError.js");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSenitize = require("express-mongo-sanitize");
+const xss = require("xss-clean")
+
+// add http security headers
+app.use(helmet());
+
+// limit requests from same api
+const limiter = rateLimit({
+  // 30 api call allowed withing one hour
+  limit: 30,
+  windowMs: 60*60*1000,
+  message: "too many requests from this IP, please try again after 1 hours"
+})
+
+// data sanitization against NoSql query injection
+app.use(mongoSenitize());
+
+// data sanitization against xss 
+app.use(xss())
+
+app.get("/testapi", limiter, (req, res) => {
+  res.status(200).json({status: "success", message: "api working"})
+})
 
 app.use("/api/user", userRoute)
 
